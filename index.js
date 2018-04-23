cc81e74b10c146407bf38bfeee638a14a26702a6
dev-test-kamal <38320734+dev-test-kamal@users.noreply.github.com>
2018-04-23 16:03:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://192.168.1.31:4000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://testtranslate.azurewebsites.net/index.html');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
